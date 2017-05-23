8cf81eb56fa9c291d846c8514d5e5e7f4dbdea13
Watchanon Numnam <jiramot@gmail.com>
2017-05-23 17:10:47 +0700

    Explore rest service only find on Location model

--- server/models/location.js
+++ server/models/location.js
@@ -1,5 +1,7 @@
 'use strict';
 
-module.exports = function(Location) {
+const utils = require('../utils/utils')
 
+module.exports = function(Location) {
+  utils.setMethodsVisibility(Location, ['find'])
 };
---
+++ server/utils/utils.js
@@ -0,0 +1,14 @@
+'use strict'
+
+module.exports.setMethodsVisibility = function (Model, methods) {
+  methods = methods || [];
+  Model.sharedClass.methods().forEach(function (method) {
+    method.shared = methods.indexOf(method.name) > -1;
+  });
+};
+
+module.exports.showMethodsVisibility = function (Model) {
+  Model.sharedClass.methods().forEach(function (method) {
+    console.log(method.name + " : " + method.shared)
+  });
+};
